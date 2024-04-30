c25f6385560f5e4ab8632ca9845e682a59875177
Goldinsvitlana <tadzhiievas@gmail.com>
2024-04-30 22:36:58 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, {Request, Response} from 'express'
+import bodyParser from 'body-parser'
+import { productsRouter } from './routers/products-router'
+import { addressesRouter } from './routers/addresses-router'
+
+const app = express()
+const port = 3000
+
+
+
+
+const parserMiddleware = bodyParser({})
+app.use(parserMiddleware)
+
+app.use('/products', productsRouter)
+app.use('/addresses', addressesRouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
